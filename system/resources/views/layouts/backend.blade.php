3435818af7ce4a6559b4810722e36566feb67ee6
kimjose <kimjose693@gmail.com>
2025-06-26 15:47:06 +0300

    links in assets folder

--- system/resources/views/layouts/backend.blade.php
+++ system/resources/views/layouts/backend.blade.php
@@ -15,7 +15,9 @@
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
     <!-- Vendor CSS Files -->
@@ -52,29 +54,42 @@
     @yield('scripts')
     @include('includes.footer')
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
     <!-- JQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Toastr JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+
     <!-- Vendor JS Files -->
-    <script src="{{ url('system/public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ url('system/public/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-    <script src="{{ url('system/public/assets/vendor/chart.js/chart.umd.js') }}"></script>
-    <script src="{{ url('system/public/assets/vendor/echarts/echarts.min.js') }}"></script>
-    <script src="{{ url('system/public/assets/vendor/quill/quill.min.js') }}"></script>
-    <script src="{{ url('system/public/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-    <script src="{{ url('system/public/assets/vendor/tinymce/tinymce.min.js') }}"></script>
-    <script src="{{ url('system/public/assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('assets/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/quill/quill.min.js') }}"></script>
+
+    <script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
 
     <!-- Template Main JS File -->
     <script src="{{ url('system/public/assets/js/main.js') }}"></script>
-    {{-- <script src="{{ url('system/public/assets/js/dashboardCount.js') }}"
-            data-patients-count-url="{{ route('patients.count') }}"
-            data-physicians-count-url="{{ route('physicians.count') }}"
-            data-referrals-count-url="{{ route('referrals.count') }}"></script> --}}
+    {{--
+    <script src="{{ url('system/public/assets/js/dashboardCount.js') }}"
+        data-patients-count-url="{{ route('patients.count') }}"
+        data-physicians-count-url="{{ route('physicians.count') }}"
+        data-referrals-count-url="{{ route('referrals.count') }}"></script> --}}
 
 </body>
 
